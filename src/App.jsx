12109540e33ebabead6f231f9e2e4a98dca6bbf2
Wesbert Edouard <wesbertjr@gmail.com>
2024-12-11 00:31:45 +0000

    simplified event listening

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import { CORE_CONCEPTS } from './data.js';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
+
+  function handleSelect() {
+    console.log('Hello World! - selected');
+}
+
   return (
     <div>
       <Header/>
@@ -23,10 +28,10 @@ function App() {
       <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
           </menu>
       </section>
       </main>
