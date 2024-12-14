cb1ebefd64e87a120f6640898158914bec8c4ee7
Wesbert Edouard <wesbertjr@gmail.com>
2024-12-14 17:10:40 +0000

    utilizing  .map to dynamically list CORE_CONCEPTS array

--- src/App.jsx
+++ src/App.jsx
@@ -20,13 +20,7 @@ function App() {
       <section id="core-concepts">
         <h2>Core Concepts</h2>
         <ul>
-          <CoreConcepts
-            title={CORE_CONCEPTS[0].title}
-            description={CORE_CONCEPTS[0].description}
-            image={CORE_CONCEPTS[0].image}
-            />
-          <CoreConcepts {...CORE_CONCEPTS[1]}/>
-          <CoreConcepts {...CORE_CONCEPTS[2]}/>
+          {CORE_CONCEPTS.map((conceptItem) => <CoreConcepts {...conceptItem}/>)}
         </ul>
       </section>
       <section id="examples">
