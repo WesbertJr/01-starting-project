a804d92a6d5f9fe45d867af48dd5c0b278b627f5
Wesbert Edouard <wesbertjr@gmail.com>
2024-12-18 04:57:50 +0000

    test

--- src/components/Examples/Examples.jsx
+++ src/components/Examples/Examples.jsx
@@ -15,7 +15,7 @@ export default function Examples(){
         <h2>Examples</h2>
         <menu>
           <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-          <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+          <Tab isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
           <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
           <TabButton  isSelected={selectedTopic === 'state'}onSelect={() => handleSelect('state')}>State</TabButton>
         </menu>
